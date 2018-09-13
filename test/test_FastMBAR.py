e333f54379b41eabd903529aa99f54e8f88327b4
Xinqiang Ding <xqding@umich.edu>
2018-09-12 20:29:58 -0400

    add test

---
+++ test/test_FastMBAR.py
@@ -0,0 +1,56 @@
+from FastMBAR import *
+
+def test_FastMBAR():
+    import numpy as np
+
+    ## draw samples from multiple states, each of which is a harmonic
+    ## osicillator
+    np.random.seed(0)
+    num_states_nz = 10 ## num of states with nonzero num of samples
+    num_states_z = 2 ## num of states with zero samples
+    num_states = num_states_nz + num_states_z
+    num_conf = np.array([400 for i in range(num_states_nz)] +
+                        [0 for i in range(num_states_z)])
+    mu = np.random.normal(0, 4, size = num_states) ## equalibrium positions
+    sigma = np.random.uniform(1, 3, size = num_states) ## sqrt of 1/k
+
+    ## draw samples from each state and calculate energies of each sample
+    ## in all states
+    Xs = []
+    for i in range(num_states):
+        Xs += list(np.random.normal(loc = mu[i],
+                                    scale = sigma[i],
+                                    size = num_conf[i]))
+    Xs = np.array(Xs)
+
+    energy = np.zeros((num_states, len(Xs)))
+    for i in range(num_states):
+        energy[i,:] = 0.5 * ((Xs - mu[i])/sigma[i])**2
+
+    ## reference results
+    true_result = -np.log(sigma)
+    true_result = true_result - true_result[0]
+
+    energy = energy.astype(np.float64)
+
+    ## reference results
+    ref_result = np.array([ 0.0, -0.381, 0.548, 0.665,
+                            0.743, -0.388, -0.242, -0.377,
+                            -0.449, -0.286, 0.003, -0.209])
+
+    ## calcuate free energies using CPUs
+    mbar = FastMBAR(energy, num_conf)
+    F_cpu = mbar.calculate_free_energies()    
+    diff_cpu = np.sqrt(np.mean((F_cpu-ref_result)**2))
+    print("RMSD (CPU calculation and reference results): {:.2f}".format(diff_cpu))
+    assert(diff_cpu <= 0.05)
+
+    ## calculate free energies using GPUs
+    if torch.cuda.is_available():
+        mbar = FastMBAR(energy, num_conf, cuda = True)
+        F_gpu = mbar.calculate_free_energies()    
+        diff_gpu = np.sqrt(np.mean((F_gpu-ref_result)**2))        
+        print("RMSD (GPU calculation and reference results): {:.2f}".format(diff_gpu))        
+        assert(diff_gpu <= 0.05)
+    
+        
