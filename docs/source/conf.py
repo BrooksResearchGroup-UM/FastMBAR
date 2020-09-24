75f521a8986402635d17e67ed962cf62dcfdab1f
Xinqiang Ding <xqding@umich.edu>
2020-09-24 19:50:11 -0400

    add version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Xinqiang Ding'
 author = 'Xinqiang Ding'
 
 # The short X.Y version
-version = '0.9'
+version = '0.0.9'
 # The full version, including alpha/beta/rc tags
-release = '0.0.5'
+release = '0.0.9'
 
 
 # -- General configuration ---------------------------------------------------
