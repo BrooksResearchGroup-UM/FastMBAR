6ce79bcf0a4affe5a0f1b34fbd32c29283b7cdd2
Xinqiang Ding <xqding@umich.edu>
2020-09-24 19:35:24 -0400

    add logo.png

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -77,13 +77,12 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
-html_logo = './logo.eps'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {'logo': 'logo.png'}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
