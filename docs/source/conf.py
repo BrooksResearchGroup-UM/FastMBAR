1d129eb40a5364d5ed59484d84c486494b7d29c4
Xinqiang Ding <xqding@umich.edu>
2020-09-25 09:26:33 -0400

    add API doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../../FastMBAR'))
+sys.path.insert(0, os.path.abspath('../'))
 
 #sys.path.insert(0, "/Users/dingxq/Google Drive/ProjectsOnGitHub/FastMBAR")
 
